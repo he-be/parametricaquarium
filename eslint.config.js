d7a93a30315d07f181a0b367c6fcefe21f3a3cb9
masahiro hibi <masahiro.hibi@gmail.com>
2025-06-29 17:04:32 +0900

    feat: Migrate ESLint config to eslint.config.js and update .gitignore

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        project: './tsconfig.json',
+        ecmaVersion: 2022,
+        sourceType: "module",
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/explicit-function-return-type": "error",
+      "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/prefer-const": "error",
+      "@typescript-eslint/no-non-null-assertion": "error",
+    },
+  },
+];
