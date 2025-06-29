e782a80d7496e403e53a210e0e0212fd5ccdbee3
masahiro hibi <masahiro.hibi@gmail.com>
2025-06-29 17:07:03 +0900

    fix: Reconstruct eslint.config.js based on Flat Config System guide

--- eslint.config.js
+++ eslint.config.js
@@ -3,20 +3,30 @@ import pluginJs from "@eslint/js";
 import tseslint from "@typescript-eslint/eslint-plugin";
 
 export default [
+  // ESLint の推奨設定
+  pluginJs.configs.recommended,
+
+  // TypeScript ESLint の推奨設定
+  ...tseslint.configs.recommended,
+
   {
     languageOptions: {
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        // その他のグローバル変数
+      },
+      parser: tseslint.parser, // TypeScript パーサーを指定
       parserOptions: {
         project: './tsconfig.json',
         ecmaVersion: 2022,
         sourceType: "module",
       },
     },
-  },
-  pluginJs.configs.recommended,
-  tseslint.configs.recommended,
-  {
+    plugins: {
+      "@typescript-eslint": tseslint, // プラグインを登録
+    },
     rules: {
+      // カスタムルール
       "@typescript-eslint/explicit-function-return-type": "error",
       "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
       "@typescript-eslint/no-explicit-any": "error",
